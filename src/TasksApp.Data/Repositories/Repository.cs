9ad1df1640fb5ae366cd535d7e476ae40f677710
Trevor Allen <27883110+MagicTrevor@users.noreply.github.com>
2023-05-25 22:07:09 -0600

    Add and implement Repository class

---
+++ src/TasksApp.Data/Repositories/Repository.cs
@@ -0,0 +1,55 @@
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using TasksApp.Domain.Entities;
+using TasksApp.Domain.Repositories;
+
+namespace TaskApp.Data.Repositories;
+
+public class Repository<TEntity> : IRepository<TEntity> where TEntity : BaseEntity
+{
+    protected readonly DbSet<TEntity> _entities;
+
+    public Repository(DbSet<TEntity> entities) => _entities = entities;
+
+    /// <inheritdoc/>
+    public async Task CreateAsync(TEntity entity) => await _entities.AddAsync(entity);
+
+    /// <inheritdoc/>
+    public async Task<TEntity?> GetAsync(Guid id, bool shouldTrackEntity = false, params Expression<Func<TEntity, object>>[] includes)
+    {
+        var query = _entities.AsQueryable();
+
+        if (!shouldTrackEntity)
+        {
+            query = query.AsNoTracking();
+        }
+        
+        foreach (var include in includes)
+        {
+            query = query.Include(include);
+        }
+
+        return await query.FirstOrDefaultAsync(e => e.Id == id);
+    }
+
+    /// <inheritdoc/>
+    public async Task UpdateAsync(TEntity entity) => await Task.Run(() => { _entities.Update(entity); });
+
+    /// <inheritdoc/>
+    public async Task<IEnumerable<TEntity>> GetAllAsync(bool shouldTrackEntity = false, params Expression<Func<TEntity, object>>[] includes)
+    {
+        var query = _entities.AsQueryable();
+
+        if (!shouldTrackEntity)
+        {
+            query = query.AsNoTracking();
+        }
+
+        foreach (var include in includes)
+        {
+            query = query.Include(include);
+        }
+
+        return await query.ToListAsync();
+    }
+}
---
+++ src/TasksApp.Domain/Repositories/IRepository.cs
@@ -0,0 +1,33 @@
+using System.Linq.Expressions;
+
+namespace TasksApp.Domain.Repositories;
+
+public interface IRepository<TEntity> where TEntity : class
+{
+    /// <summary>
+    /// Creates a new <see cref="TEntity"/>
+    /// </summary>
+    Task CreateAsync(TEntity entity);
+
+    /// <summary>
+    /// Updates the <see cref="TEntity"/>
+    /// </summary>
+    /// <param name="entity"></param>
+    /// <returns></returns>
+    Task UpdateAsync(TEntity entity);
+
+    /// <summary>
+    /// Gets the requested <see cref="TEntity"/> if the id exists
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="includes"></param>
+    /// <returns></returns>
+    Task<TEntity?> GetAsync(Guid id, bool shouldTrackEntity = false, params Expression<Func<TEntity, object>>[] includes);
+
+    /// <summary>
+    /// Returns all ToDos
+    /// </summary>
+    /// <param name="includes"></param>
+    /// <returns></returns>
+    Task<IEnumerable<TEntity>> GetAllAsync(bool shouldTrackEntity = false, params Expression<Func<TEntity, object>>[] includes);
+}
