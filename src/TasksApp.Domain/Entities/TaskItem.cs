47682694bcf386174037cd85d2eb0e4997555a03
Trevor Allen <27883110+MagicTrevor@users.noreply.github.com>
2023-05-25 22:38:34 -0600

    Add CreatedDate to TaskItem

--- src/TasksApp.Domain/Entities/TaskItem.cs
+++ src/TasksApp.Domain/Entities/TaskItem.cs
@@ -5,6 +5,7 @@ public class TaskItem : BaseEntity
     public string Description { get; private set; }
     public bool IsComplete { get; private set; }
     public DateTime CompletedDate { get; private set; }
+    public DateTime CreatedDate { get; private set; }
 
     /// <summary>
     /// Default constructor for <see cref="TaskItem" />
@@ -19,7 +20,9 @@ public class TaskItem : BaseEntity
         }
 
         Description = description;
-        IsComplete = false;        
+        IsComplete = false;
+
+        CreatedDate = DateTime.Now;
     }
 
     /// <summary>
