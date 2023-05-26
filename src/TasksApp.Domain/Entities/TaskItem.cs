17c1d9952be7e0a928046f9ec537aa7c3b17c3ea
Trevor Allen <27883110+MagicTrevor@users.noreply.github.com>
2023-05-25 21:38:32 -0600

    Add new TaskItem entity

---
+++ src/TasksApp.Domain/Entities/TaskItem.cs
@@ -0,0 +1,36 @@
+namespace TasksApp.Domain.Entities;
+
+public class TaskItem : BaseEntity
+{
+    public string Description { get; private set; }
+    public bool IsComplete { get; private set; }
+    public DateTime CompletedDate { get; private set; }
+
+    /// <summary>
+    /// Default constructor for <see cref="TaskItem" />
+    /// </summary>
+    /// <param name="description"></param>
+    /// <exception cref="ArgumentNullException"></exception>
+    public TaskItem(string description)
+    {
+        if (string.IsNullOrWhiteSpace(description))
+        {
+            throw new ArgumentNullException(nameof(description), "Must include a description when creating a new TaskItem");
+        }
+
+        Description = description;
+        IsComplete = false;        
+    }
+
+    /// <summary>
+    /// Marks the <see cref="TaskItem" /> as complete
+    /// </summary>
+    public void CompleteTaskItem()
+    {
+        if (!IsComplete)
+        {
+            IsComplete = true;
+            CompletedDate = DateTime.Now;
+        }
+    }
+}
