f554daf3b1d90c623489f6c4752ab744bb7c6e51
Trevor Allen <27883110+MagicTrevor@users.noreply.github.com>
2023-05-26 00:14:53 -0600

    Update to setup CORS, Db, and repository registration

--- src/TasksApp.Api/Program.cs
+++ src/TasksApp.Api/Program.cs
@@ -1,11 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using TasksApp.Data;
+using TasksApp.Data.Repositories;
+using TasksApp.Domain.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
+{
+    builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+}));
+
+// Configure the Database connection.
+builder.Services.AddDbContext<TasksAppContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
+
+// Register repositories
+builder.Services.AddScoped<ITaskItemRepository, TaskItemRepository>();
+
+// Register services
 
 var app = builder.Build();
 
@@ -18,8 +37,17 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors("corsapp");
+
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Make sure database is up to date
+using (var scope = app.Services.CreateScope())
+{
+    var tasksContext = scope.ServiceProvider.GetRequiredService<TasksAppContext>();
+    tasksContext.Database.EnsureCreated();
+}
 
 app.Run();
