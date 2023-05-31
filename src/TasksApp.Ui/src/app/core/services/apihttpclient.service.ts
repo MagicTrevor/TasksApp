a86d022c2175bf40d044fe09d9654bb74e600617
Trevor Allen <27883110+MagicTrevor@users.noreply.github.com>
2023-05-31 00:40:34 -0600

    Update service to auto login

--- src/TasksApp.Ui/src/app/core/services/apihttpclient.service.ts
+++ src/TasksApp.Ui/src/app/core/services/apihttpclient.service.ts
@@ -1,24 +1,32 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { TaskItem } from "../models/task-item";
+import { AuthService } from "src/app/shared/auth.service";
+import { User } from "src/app/shared/user";
 
 @Injectable()
 export class ApiHttpClientService {
-    constructor(private http: HttpClient) { }
+    constructor(private auth: AuthService, private http: HttpClient) {
+        var user = new User();
+        user.username = 'test';
+        user.password = 'test';
+
+        this.auth.login(user);
+    }
 
     public getTaskItems() {
-        return this.http.get<TaskItem[]>("http://localhost:8080/api/taskitems/");
+        return this.http.get<TaskItem[]>("http://localhost:5266/api/tasks/");
     }
 
     public create(item: TaskItem) {
-        return this.http.post<TaskItem>("http://localhost:8080/api/taskitems/", item);
+        return this.http.post<TaskItem>("http://localhost:5266/api/tasks/", item);
     }
 
     public updateDescription(id: string, description: string) {
-        return this.http.post<TaskItem>(`http://localhost:8080/api/taskitems/${id}/description?description=$(description)`, null);
+        return this.http.put<TaskItem>(`http://localhost:5266/api/tasks/${id}/description?description=$(description)`, null);
     }
 
     public complete(id: string) {
-        return this.http.post<TaskItem>(`http://localhost:8080/api/taskitems/${id}/complete`, null);
+        return this.http.put<TaskItem>(`http://localhost:5266/api/tasks/${id}/complete`, null);
     }
 }
